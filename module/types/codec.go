91b8bf487c53721ada4dd1f7eba220765f7457d1
Mario Karagiorgas <salem8@gmail.com>
2021-02-26 19:19:50 +0200

    Migrate codec

--- module/types/codec.go
+++ module/types/codec.go
@@ -2,17 +2,41 @@ package types
 
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-var ModuleCdc = codec.NewAminoCodec(codec.NewLegacyAmino())
+var (
+	amino = codec.NewLegacyAmino()
 
-func init() {
-	RegisterCodec(ModuleCdc)
-}
+	// ModuleCdc references the module codec. Note, the codec should
+	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
+	// still used for that purpose.
+	ModuleCdc = codec.NewAminoCodec(amino)
+)
 
 // RegisterCodec registers concrete types on amino
-func RegisterCodec(cdc *codec.AminoCodec) {
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
 	cdc.RegisterConcrete(MsgCreateAtomicSwap{}, "bep3/MsgCreateAtomicSwap", nil)
 	cdc.RegisterConcrete(MsgRefundAtomicSwap{}, "bep3/MsgRefundAtomicSwap", nil)
 	cdc.RegisterConcrete(MsgClaimAtomicSwap{}, "bep3/MsgClaimAtomicSwap", nil)
+}
+
+func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgCreateAtomicSwap{},
+		&MsgRefundAtomicSwap{},
+		&MsgClaimAtomicSwap{},
+	)
+}
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+	authtypes.RegisterLegacyAminoCodec(amino)
+	banktypes.RegisterLegacyAminoCodec(amino)
+	amino.Seal()
 }
