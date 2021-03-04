9a659aab823599cdaa55189a8ab0ea61a1b05755
Mario Karagiorgas <salem8@gmail.com>
2021-03-04 13:43:01 +0200

    Register module encoders

--- module/encoding.go
+++ module/encoding.go
@@ -4,15 +4,19 @@ import (
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/std"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/auth/legacy/legacytx"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
+	"github.com/cosmos/cosmos-sdk/x/bank"
 )
 
 // EncodingConfig specifies the concrete encoding types to use for a given app.
 // This is provided for compatibility between protobuf and amino implementations.
 type EncodingConfig struct {
 	InterfaceRegistry types.InterfaceRegistry
-	Marshaler         codec.Marshaler
+	Marshaller        codec.Marshaler
 	TxConfig          client.TxConfig
 	Amino             *codec.LegacyAmino
 }
@@ -27,36 +31,60 @@ func MakeEncodingConfig() EncodingConfig {
 func MakeAminoEncodingConfig() EncodingConfig {
 	cdc := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
-	marshaler := getAminoMarshaller(cdc)
+	marshaller := getAminoMarshaller(cdc)
 
-	return EncodingConfig{
+	cfg := EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
-		Marshaler:         marshaler,
+		Marshaller:        marshaller,
 		TxConfig:          legacytx.StdTxConfig{Cdc: cdc},
 		Amino:             cdc,
 	}
+
+	std.RegisterLegacyAminoCodec(cfg.Amino)
+	std.RegisterInterfaces(cfg.InterfaceRegistry)
+	ModuleBasics := module.NewBasicManager(
+		bank.AppModuleBasic{},
+		auth.AppModuleBasic{},
+	)
+
+	ModuleBasics.RegisterLegacyAminoCodec(cfg.Amino)
+	ModuleBasics.RegisterInterfaces(cfg.InterfaceRegistry)
+
+	return cfg
 }
 
 func getAminoMarshaller(cdc *codec.LegacyAmino) *codec.AminoCodec {
-	marshaler := codec.NewAminoCodec(cdc)
-	return marshaler
+	marshaller := codec.NewAminoCodec(cdc)
+	return marshaller
 }
 
 // MakeProtoEncodingConfig creates an EncodingConfig for a protobuf based configuration.
 func MakeProtoEncodingConfig() EncodingConfig {
 	cdc := codec.NewLegacyAmino()
 	interfaceRegistry := types.NewInterfaceRegistry()
-	marshaler := getProtoMarshaller(interfaceRegistry)
+	marshaller := getProtoMarshaller(interfaceRegistry)
 
-	return EncodingConfig{
+	cfg := EncodingConfig{
 		InterfaceRegistry: interfaceRegistry,
-		Marshaler:         marshaler,
-		TxConfig:          tx.NewTxConfig(marshaler, tx.DefaultSignModes),
+		Marshaller:        marshaller,
+		TxConfig:          tx.NewTxConfig(marshaller, tx.DefaultSignModes),
 		Amino:             cdc,
 	}
+
+	std.RegisterLegacyAminoCodec(cfg.Amino)
+	std.RegisterInterfaces(cfg.InterfaceRegistry)
+	ModuleBasics := module.NewBasicManager(
+		bank.AppModuleBasic{},
+		auth.AppModuleBasic{},
+	)
+
+	ModuleBasics.RegisterLegacyAminoCodec(cfg.Amino)
+	ModuleBasics.RegisterInterfaces(cfg.InterfaceRegistry)
+
+	return cfg
 }
 
 func getProtoMarshaller(interfaceRegistry types.InterfaceRegistry) *codec.ProtoCodec {
-	marshaler := codec.NewProtoCodec(interfaceRegistry)
-	return marshaler
+	marshaller := codec.NewProtoCodec(interfaceRegistry)
+	return marshaller
 }
